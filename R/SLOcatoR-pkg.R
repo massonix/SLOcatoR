f4e27bf0cdba5f292b1c79c60b709a72a6e20ff3
Federico Marini <marinif@uni-mainz.de>
2023-06-14 12:22:45 +0200

    update content of pkg-level docs

--- R/SLOcatoR-pkg.R
+++ R/SLOcatoR-pkg.R
@@ -1,6 +1,10 @@
 #' SLOcatoR
 #'
-#' `SLOcatoR` is a Bioconductor package that provides 
+#' `SLOcatoR` is a Bioconductor package that provides TODO TODO
+#' 
+#' TODO TODO something more
+#' 
+#' Can contain quite a lot of things in here, remove once done
 #'
 #' @name SLOcatoR-pkg
 #' @docType package
